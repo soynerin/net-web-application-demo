bd7bea368ed8230ed30ca5987be3194d43d6efa8
Neri Espinola <neri.agustin.es@outlook.com>
2021-12-31 18:25:05 -0300

    Inicio de desarrollo

---
+++ WebApplicationDemo/Controllers/AutoresController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationDemo.Entidades;
+
+namespace WebApplicationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() { new Autor() { Id = 1, Nombre = "Neri" } }; 
+        }
+    }
+}
---
+++ WebApplicationDemo/Entidades/Autor.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplicationDemo.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
